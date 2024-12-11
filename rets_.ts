2c4064ea21301f05c926b7479347e56b699d54b3
eujin31 <eujin31@gmail.com>
2024-12-11 23:53:41 +0900

    feat: 수익률 데이터 수정

---
+++ rets_.ts
@@ -0,0 +1,135 @@
+rets: AutoOrderedDict({
+        'total': AutoOrderedDict({
+            'total': 12,
+            'open': 0,
+            'closed': 12
+        }),
+        'streak': AutoOrderedDict({
+            'won': AutoOrderedDict({
+                'current': 0,
+                'longest': 6
+            }),
+            'lost': AutoOrderedDict({
+                'current': 1,
+                'longest': 3
+            })
+        }),
+        'pnl': AutoOrderedDict({
+            'gross': AutoOrderedDict({
+                'total': -123.90144348144531,
+                'average': -10.325120290120443
+            }),
+            'net': AutoOrderedDict({
+                'total': -123.90144348144531,
+                'average': -10.325120290120443
+            })
+        }),
+        'won': AutoOrderedDict({
+            'total': 7,
+            'pnl': AutoOrderedDict({
+                'total': 73.14407348632812,
+                'average': 10.449153355189733,
+                'max': 30.806121826171875
+            })
+        }),
+        'lost': AutoOrderedDict({
+            'total': 5,
+            'pnl': AutoOrderedDict({
+                'total': -197.04551696777344,
+                'average': -39.40910339355469,
+                'max': -105.50003051757812
+            })
+        }),
+        'long': AutoOrderedDict({
+            'total': 6,
+            'pnl': AutoOrderedDict({
+                'total': -39.399871826171875,
+                'average': -6.5666453043619795,
+                'won': AutoOrderedDict({
+                    'total': 16.400146484375,
+                    'average': 4.10003662109375,
+                    'max': 5.7000732421875
+                }),
+                'lost': AutoOrderedDict({
+                    'total': -55.800018310546875,
+                    'average': -27.900009155273438,
+                    'max': -43.79997253417969
+                })
+            }),
+            'won': 4,
+            'lost': 2
+        }),
+        'short': AutoOrderedDict({
+            'total': 6,
+            'pnl': AutoOrderedDict({
+                'total': -84.50157165527344,
+                'average': -14.083595275878906,
+                'won': AutoOrderedDict({
+                    'total': 56.743927001953125,
+                    'average': 18.914642333984375,
+                    'max': 30.806121826171875
+                }),
+                'lost': AutoOrderedDict({
+                    'total': -141.24549865722656,
+                    'average': -47.08183288574219,
+                    'max': -105.50003051757812
+                })
+            }),
+            'won': 3,
+            'lost': 3
+        }),
+        'len': AutoOrderedDict({
+            'total': 40,
+            'average': 3.3333333333333335,
+            'max': 7,
+            'min': 1,
+            'won': AutoOrderedDict({
+                'total': 15,
+                'average': 2.142857142857143,
+                'max': 5,
+                'min': 1
+            }),
+            'lost': AutoOrderedDict({
+                'total': 25,
+                'average': 5.0,
+                'max': 7,
+                'min': 2
+            }),
+            'long': AutoOrderedDict({
+                'total': 20,
+                'average': 3.3333333333333335,
+                'max': 7,
+                'min': 1,
+                'won': AutoOrderedDict({
+                    'total': 8,
+                    'average': 2.0,
+                    'max': 4,
+                    'min': 1
+                }),
+                'lost': AutoOrderedDict({
+                    'total': 12,
+                    'average': 6.0,
+                    'max': 7,
+                    'min': 5
+                })
+            }),
+            'short': AutoOrderedDict({
+                'total': 20,
+                'average': 3.3333333333333335,
+                'max': 7,
+                'min': 1,
+                'won': AutoOrderedDict({
+                    'total': 7,
+                    'average': 2.3333333333333335,
+                    'max': 5,
+                    'min': 1
+                }),
+                'lost': AutoOrderedDict({
+                    'total': 13,
+                    'average': 4.333333333333333,
+                    'max': 7,
+                    'min': 2
+                })
+            })
+        })
+    })
